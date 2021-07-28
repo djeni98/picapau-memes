e4d106f6f60e4627da21acbc467d260ff08dd466
Djenifer R. P <djeniferrenata@yahoo.com.br>
2021-07-28 11:01:19 -0300

    Cria componente SelectItem

---
+++ Picapau Memes/Components/CustomUIView.swift
@@ -0,0 +1,40 @@
+//
+//  CustomUIView.swift
+//  Picapau Memes
+//
+//  Created by Djenifer Renata Pereira on 28/07/21.
+//
+
+import Foundation
+import UIKit
+
+class CustomUIView: UIView {
+    var view: UIView!
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        loadViewFromNib()
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadViewFromNib()
+    }
+
+    func loadViewFromNib() {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: String(describing: type(of: self)), bundle: bundle)
+        let view = nib.instantiate(withOwner: self, options: nil).first as! UIView
+        view.frame = bounds
+        view.autoresizingMask = [
+            UIView.AutoresizingMask.flexibleWidth,
+            UIView.AutoresizingMask.flexibleHeight
+        ]
+
+        addSubview(view)
+        self.view = view
+
+        customizeView()
+    }
+
+    func customizeView() {}
+}
---
+++ Picapau Memes/Components/SelectItem/SelectItemView.swift
@@ -0,0 +1,33 @@
+//
+//  SelectItemView.swift
+//  Picapau Memes
+//
+//  Created by Djenifer Renata Pereira on 28/07/21.
+//
+
+import UIKit
+
+class SelectItemView: CustomUIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+    @IBOutlet weak var circleImageView: UIImageView!
+    var selected = false
+    func toggleSelection() {
+        selected.toggle()
+        let imageName = selected ? "checkmark.circle.fill" : "circle"
+        circleImageView.image = UIImage(systemName: imageName)
+    }
+
+    override func customizeView() {
+        self.view.layer.cornerRadius = 10
+        self.view.layer.borderWidth = 2
+        self.view.layer.borderColor = UIColor(named: "AppRed")?.cgColor.copy(alpha: 0.5)
+    }
+}
