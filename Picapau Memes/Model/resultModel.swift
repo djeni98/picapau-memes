9821f6676e727fbb274436ccfacc8ce4f3a8c198
Djenifer R. P <djeniferrenata@yahoo.com.br>
2021-07-29 18:59:59 -0300

    Cria modelo para resultados e obtem informações na tela ResultVC

---
+++ Picapau Memes/Model/resultModel.swift
@@ -0,0 +1,52 @@
+//
+//  resultModel.swift
+//  Picapau Memes
+//
+//  Created by Djenifer Renata Pereira on 29/07/21.
+//
+
+import Foundation
+import UIKit
+
+struct Background {
+    var colorName: String
+    var alpha: CGFloat = 1
+}
+
+struct Result {
+    var imageName: String
+    var description: String
+    var background: [Background]? = nil
+
+    func getGradientLayer(frame: CGRect) -> CAGradientLayer? {
+        guard let bg = background else { return nil }
+        if bg.count != 2 { return nil }
+
+        print(frame)
+        let colorTop = UIColor(named: bg[0].colorName)!.cgColor.copy(alpha: bg[0].alpha)!
+        let colorBottom = UIColor(named: bg[1].colorName)!.cgColor.copy(alpha: bg[1].alpha)!
+
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [colorTop, colorBottom]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.frame = frame
+
+        return gradientLayer
+    }
+}
+
+let resultsArrayData = [
+    Result(
+        imageName: "triste-nao",
+        description: "Triste, não?",
+        background: [Background(colorName: "AppBlue", alpha: 0.5)]
+    ),
+    Result(
+        imageName: "aeee",
+        description: "Aeeeeeeeee",
+        background: [
+            Background(colorName: "AppYellow", alpha: 0.9),
+            Background(colorName: "AppRed", alpha: 0.8),
+        ]
+    ),
+]
--- Picapau Memes/Result/ResultViewController.swift
+++ Picapau Memes/Result/ResultViewController.swift
@@ -8,7 +8,11 @@
 import UIKit
 
 class ResultViewController: UIViewController {
-    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var percentLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var descriptonLabel: UILabel!
+
+    @IBOutlet weak var coloredBackgroundView: UIView!
 
     var resultPercent: Int? = nil
 
@@ -16,8 +20,37 @@ class ResultViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        if let percent = resultPercent {
-            resultLabel.text = "\(percent) %"
+        coloredBackgroundView.alpha = 0
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        setupImageAndLabels()
+        UIView.animate(withDuration: 0.2, animations: {
+            self.coloredBackgroundView.alpha = 1
+        })
+    }
+
+    func setupImageAndLabels() {
+        guard let percent = resultPercent else { return }
+
+        percentLabel?.text = "\(percent) %"
+        percentLabel?.font = .rounded(ofSize: 48, weight: .black)
+
+        let decimalPercent = Double(percent) / 100
+        let resultArrayIndex = Int(decimalPercent * Double(resultsArrayData.count)) - 1
+        let result = resultsArrayData[resultArrayIndex]
+
+        imageView?.image = UIImage(named: result.imageName)
+        descriptonLabel?.text = result.description
+        descriptonLabel?.font = .rounded(ofSize: 27, weight: .bold)
+
+        if let gradientLayer = result.getGradientLayer(frame: coloredBackgroundView.bounds) {
+            coloredBackgroundView.backgroundColor = nil
+            coloredBackgroundView.layer.insertSublayer(gradientLayer, at: 0)
+        } else if let background = result.background?.first {
+            let color = UIColor(named: background.colorName)?.withAlphaComponent(background.alpha)
+            coloredBackgroundView.backgroundColor = color
         }
     }
 
